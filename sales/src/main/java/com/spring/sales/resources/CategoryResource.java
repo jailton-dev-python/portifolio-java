56189e971cceb42ec35dcf367ebef1058440a944
Jailton <jailton.dev.python@gmail.com>
2023-12-15 18:01:11 -0300

    finalizado resources e services
    categoria
    pedido
    produto

---
+++ sales/src/main/java/com/spring/sales/resources/CategoryResource.java
@@ -0,0 +1,33 @@
+package com.spring.sales.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.sales.entities.Category;
+import com.spring.sales.service.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ sales/src/main/java/com/spring/sales/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.spring.sales.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.sales.entities.Product;
+import com.spring.sales.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
