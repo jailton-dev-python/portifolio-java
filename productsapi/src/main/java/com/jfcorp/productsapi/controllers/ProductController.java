9ff7313673873b93df771f8d496455d18005384e
Jailton <jailton.dev.python@gmail.com>
2023-12-26 10:32:07 -0300

    implementado delete

--- productsapi/src/main/java/com/jfcorp/productsapi/controllers/ProductController.java
+++ productsapi/src/main/java/com/jfcorp/productsapi/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +61,15 @@ public class ProductController {
 		var productModel = new ProductModel();
 		BeanUtils.copyProperties(productRecordDto, productModel);
 		return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id) {
+		Optional<ProductModel> productO = productRepository.findById(id);		
+		if (productO.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");			
+		}		
+		productRepository.delete(productO.get());		
+		return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
 	}
 }
