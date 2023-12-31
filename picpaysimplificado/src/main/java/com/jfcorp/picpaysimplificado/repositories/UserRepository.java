00db0a42ceff1154b5c5003e772f8fba7891288d
Jailton <jailton.dev.python@gmail.com>
2023-12-31 14:08:34 -0300

    create user repository
    create user repository

---
+++ picpaysimplificado/src/main/java/com/jfcorp/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jfcorp.picpaysimplificado.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jfcorp.picpaysimplificado.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findUserByDocument(String document);
+	Optional<User> findUserById(Long id);
+}
