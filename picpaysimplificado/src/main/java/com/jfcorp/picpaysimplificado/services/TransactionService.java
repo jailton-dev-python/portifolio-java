991e3fc359cc63c2d688394f4ea9c3278705095a
Jailton <jailton.dev.python@gmail.com>
2024-01-04 08:15:49 -0300

    correção no import do Map

--- picpaysimplificado/src/main/java/com/jfcorp/picpaysimplificado/services/TransactionService.java
+++ picpaysimplificado/src/main/java/com/jfcorp/picpaysimplificado/services/TransactionService.java
@@ -3,7 +3,8 @@ package com.jfcorp.picpaysimplificado.services;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import org.hibernate.mapping.Map;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -61,18 +62,17 @@ public class TransactionService {
 	}
 	
 	public boolean authorizeTransaction(User sender, BigDecimal value) {
-		return true;
-		/*
+		//return true;
+		
 		ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
 				
 		if(authorizationResponse.getStatusCode() == HttpStatus.OK) {			
-			//String message = authorizationResponse.getBody().get("message");
-			String message = "Autorizado";
-			//System.out.println(message);
-			return "Autorizado".equalsIgnoreCase(message);	        
+			//String message = (String) authorizationResponse.getBody().get("message");
+			boolean message = (boolean) authorizationResponse.getBody().get("message");
+			//return "Autorizado".equalsIgnoreCase(message);
+			return message;
 		} else {
 			return false;
 		}
-		*/
 	}
 }
